9ef926a48df34ae13264f85645e634c882dbfbdc
Subin_Moon <joy120594@gmail.com>
2021-08-29 23:09:22 -0400

    Modify import of firebase according to the version

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDK4DVNDC-yaUiNqSZdVSZBmZ3bqlK578g",
+    authDomain: "clone-twitter-44373.firebaseapp.com",
+    projectId: "clone-twitter-44373",
+    storageBucket: "clone-twitter-44373.appspot.com",
+    messagingSenderId: "427533062809",
+    appId: "1:427533062809:web:59e41adc29b3d554535e08"
+};
+
+export default firebase.initializeApp(firebaseConfig);
